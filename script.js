6d27af924bdf2625aff7dbaf1504cde44acc027d
LokeshKanagaraj10 <120785781+LokeshKanagaraj10@users.noreply.github.com>
2023-01-29 02:18:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function threeSum(arr, target) {
-  //your code here
+	let i
+	let j
+	let k
+	const mindiff = []
+	for(i=0;i<arr.length;i++){
+		for(j=i+1;j<arr.length;j++){
+			for(k=j+1;k<arr.length;k++){
+				const sum = arr[i]+arr[j]+arr[k]
+				mindiff.push(sum - target)
+			}
+		}
+	}
+	const min = mindiff[0]
+	let p
+	for(p=1;p<mindiff.length;p++){
+	  if(min > mindiff[p]){
+	    min = mindiff[p]
+	  }
+	}
+	return min
 }
 
 module.exports = threeSum;
